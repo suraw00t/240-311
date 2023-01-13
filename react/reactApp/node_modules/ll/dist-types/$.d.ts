2d40362a0e982fc229ad949dc34c5076e967aaee
Surawut Saithong-in <surawut.kong@gmail.com>
2023-01-13 18:57:22 +0700

    remove

--- react/reactApp/node_modules/ll/dist-types/$.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function $<K extends keyof HTMLElementTagNameMap>(selectors: K, parent?: HTMLElement | Document): HTMLElementTagNameMap[K] | null;
-export declare function $<K extends keyof SVGElementTagNameMap>(selectors: K, parent?: SVGElement | Document): SVGElementTagNameMap[K] | null;
-export declare function $<E extends Element = HTMLElement>(selectors: string, parent?: Element | Document): E | null;
--- react/reactApp/node_modules/ll/dist-types/htmlEncode.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function htmlEncode(value: string): string;
